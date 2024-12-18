7f813774871623eb51590bb24e236d6b6c45b32c
Bikky Kumar <35852296+bikky-kumar@users.noreply.github.com>
2024-12-18 13:36:27 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+// server.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
